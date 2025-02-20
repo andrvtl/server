73a2e49db7520535779edf779380364a2f4617ff
Andrea Vitale <andreavitale.scuola@gmail.com>
2025-02-20 23:08:39 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -33,6 +33,8 @@ public class Main {
             System.out.println("Client num."+i);
 
             s.attendi();
+            s.scrivi();
+            s.leggi();
             s.chiudi();
             i++;
         }
